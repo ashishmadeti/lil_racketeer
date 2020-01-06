bbf5d99dd407ce93322662f1594c93ffc792cfcb
Ashish Madeti <ashishmadeti@gmail.com>
2020-01-06 23:46:24 +0530

    Simplify 'insertL' by replacing `(cons old (cdr lat))` with `lat`

--- c3/insertL.rkt
+++ c3/insertL.rkt
@@ -4,7 +4,7 @@
     (lambda (new old lat)
         (cond
             [(null? lat) '()]
-            [(eq? (car lat) old) (cons new (cons old (cdr lat)))]
+            [(eq? (car lat) old) (cons new lat)]
             [else (cons (car lat) (insertL new old (cdr lat)))])))
 
 ; examples
