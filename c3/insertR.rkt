23dc000f2dcca08cd04d78cfc435420ec421aac2
Ashish Madeti <ashishmadeti@gmail.com>
2020-01-06 23:26:21 +0530

    Define 'insertR' that inserts `new` after `old` in the list `lat`

---
+++ c3/insertR.rkt
@@ -0,0 +1,17 @@
+#lang racket
+
+(define insertR
+    (lambda (new old lat)
+        (cond
+            [(null? lat) '()]
+            [(eq? (car lat) old) (cons (car lat) (cons new (cdr lat)))]
+            [else (cons (car lat) (insertR new old (cdr lat)))])))
+
+; examples
+(insertR 'pie 'apple (list 'I 'want 'apple 'and 'milk))
+
+(insertR 'tube 'light (list 'No 'electricity 'today))
+
+(insertR 'not 'does (list 'Ron 'does 'believe 'in 'government))
+
+(insertR 'a 'b '())
